aaafc95d7f2f687506090fa0909ee5721f04ed2e
awojciak <48929607+awojciak@users.noreply.github.com>
2019-03-30 20:29:43 +0100

    Source files

---
+++ mysteries.js
@@ -0,0 +1,17 @@
+const mysteries = [['STEFAN ŻEROMSKI', 'pisarze'],
+['LEONARDO DA VINCI', 'wynalazcy'],
+['GRANICA', 'pojęcia'],
+['TWIERDZENIE PITAGORASA', 'matematyka'],
+['TO BE OR NOT TO BE', 'cytaty'],
+['CORN FLAKES', 'jedzenie'],
+['SUPERMAN', 'komiksy'],
+['ŻALUZJE', 'przedmioty'],
+['WODA MINERALNA', 'napoje'],
+['IRON MAN', 'komiksy'],
+['DISCO POLO', 'muzyka'],
+['BIBLIA', 'religia'],
+['THOMAS EDISON', 'wynalazcy'],
+['FORREST GUMP', 'film']
+];
+
+export default mysteries;
